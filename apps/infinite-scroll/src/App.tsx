791db180d36e1ea44025e4f5ca220960558736ae
yeshvinash <avinashkhara12@gmail.com>
2026-01-07 17:35:50 +0530

    Project setup

---
+++ apps/infinite-scroll/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@repo/ui";
+
+function App() {
+  return (
+    <div>
+      <h1>RTK CRUD App</h1>
+      <Button>Shared Button</Button>
+    </div>
+  );
+}
+
+export default App;
